6aaedc9b246ae90f58b5276dc7c2a5f8bf8ca9bc
Juan Paganuzzi <paganuzzi@gmail.com>
2018-12-30 01:09:32 -0300

    prueba merge

--- ArrayTest.php
+++ ArrayTest.php
@@ -5,6 +5,6 @@ class ArrayHasKeyTest extends TestCase
 {
     public function testFailure()
     {
-        $this->assertArrayHasKey('bar', ['bar' => 'baz']);
+        $this->assertArrayHasKey('baw', ['bar' => 'baz']);
     }
 }
