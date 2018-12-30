3f40af848e7ea259322fdd2d5e73fd4574e9095d
Juan Paganuzzi <paganuzzi@gmail.com>
2018-12-30 00:13:19 -0300

    ahora anda

---
+++ ArrayTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ArrayHasKeyTest extends TestCase
+{
+    public function testFailure()
+    {
+        $this->assertArrayHasKey('bar', ['bar' => 'baz']);
+    }
+}
